ab0b55821894addc2d0412b48332eabc550e539d
김명성 <kms@stunitas.com>
2021-03-17 12:19:02 +0900

    ci 테스트

--- app/src/main/java/beeasy/a90ms/actionsample/MainActivity.kt
+++ app/src/main/java/beeasy/a90ms/actionsample/MainActivity.kt
@@ -2,10 +2,17 @@ package beeasy.a90ms.actionsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast()
+    }
+
+    private fun showToast() {
+        Toast.makeText(this,"text",Toast.LENGTH_LONG).show()
     }
 }
